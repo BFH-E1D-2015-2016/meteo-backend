5c12993a1571152535f5eb90dc4f03fdd6af4ae4
Samuel Dolt <samuel@dolt.ch>
2016-12-27 16:08:15 +0100

    Update

---
+++ src/create_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE tmp (
+                  id              INTEGER PRIMARY KEY,
+                  timestmp        INTEGER NOT NULL,
+                  value           INTEGER NOT NULL,
+)
