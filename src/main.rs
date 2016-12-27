bb83a05512da2af073d7fdc16004e18941c4b9e2
Samuel Dolt <samuel@dolt.ch>
2016-12-27 15:09:06 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
 extern crate glob;
 extern crate regex;
 
+use std::str::FromStr;
 use std::path::PathBuf;
 use regex::Regex;
 use std::fs::File;
 use std::io::Read;
 
-fn search_sensors() -> Vec<PathBuf> {
+fn read_sensor() -> Result<f32, ()> {
 
-    glob::glob("/sys/bus/w1/devices/28-*/w1_slave").unwrap().filter_map(Result::ok).collect()
-}
-
-fn main() {
     let data = search_sensors();
     for path in data {
         let mut file = File::open(path).unwrap();
@@ -19,18 +16,30 @@ fn main() {
 
         let re_crc = Regex::new(r"(YES)").unwrap();
 
-        file.read_to_end(&mut text);
+        let _ = file.read_to_end(&mut text);
 
         if re_crc.is_match(std::str::from_utf8(&text).unwrap()) {
             let re_temp = Regex::new(r"t=(-?\d+)").unwrap();
 
             match re_temp.captures(std::str::from_utf8(&text).unwrap()).unwrap().at(1) {
-                Some(tmp) => println!("{}", tmp),
+                Some(tmp) => {
+                    let temperature = f32::from_str(tmp).unwrap();
+                    return Ok(temperature / 1000.0);
+                }
                 None => {}
             };
         }
-
-
     }
-    println!("{:?}", search_sensors());
+
+    Err(())
+
+}
+fn search_sensors() -> Vec<PathBuf> {
+
+    glob::glob("/sys/bus/w1/devices/28-*/w1_slave").unwrap().filter_map(Result::ok).collect()
+}
+
+
+fn main() {
+    println!("{:?}", read_sensor());
 }
