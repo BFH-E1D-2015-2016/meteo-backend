2878421e9f0c65e3b09ac293ca771087cd51f913
Samuel Dolt <samuel@dolt.ch>
2016-12-27 15:30:40 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,100 @@ use regex::Regex;
 use std::fs::File;
 use std::io::Read;
 
-fn read_sensor() -> Result<f32, ()> {
+use std::error;
+use std::error::Error;
+use std::fmt::{Debug, Display};
+use std::fmt;
+
+#[derive(Debug)]
+enum TmpError {
+    IOError(std::io::Error),
+    RegexError(regex::Error),
+    FloatError(std::num::ParseFloatError),
+    Utf8Error(std::str::Utf8Error),
+    Unknow,
+}
+
+impl From<std::io::Error> for TmpError {
+    fn from(err: std::io::Error) -> TmpError {
+        TmpError::IOError(err)
+    }
+}
+
+impl From<regex::Error> for TmpError {
+    fn from(err: regex::Error) -> TmpError {
+        TmpError::RegexError(err)
+    }
+}
+
+impl From<std::num::ParseFloatError> for TmpError {
+    fn from(err: std::num::ParseFloatError) -> TmpError {
+        TmpError::FloatError(err)
+    }
+}
+
+impl From<std::str::Utf8Error> for TmpError {
+    fn from(err: std::str::Utf8Error) -> TmpError {
+        TmpError::Utf8Error(err)
+    }
+}
+
+impl Error for TmpError {
+    /// A short description of the error.
+    fn description(&self) -> &str {
+        match *self {
+            TmpError::IOError(ref err) => err.description(),
+            TmpError::FloatError(ref err) => err.description(),
+            TmpError::RegexError(ref err) => err.description(),
+            TmpError::Utf8Error(ref err) => err.description(),
+            TmpError::Unknow => "Unknow error.",
+        }
+    }
+
+    /// The lower level cause of this error, if any.
+    fn cause(&self) -> Option<&Error> {
+        match *self {
+            TmpError::IOError(ref err) => Some(err),
+            TmpError::FloatError(ref err) => Some(err),
+            TmpError::RegexError(ref err) => Some(err),
+            TmpError::Utf8Error(ref err) => Some(err),
+            TmpError::Unknow => None,
+        }
+    }
+}
+
+impl Display for TmpError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            // Both underlying errors already impl `Display`, so we defer to
+            // their implementations.
+            TmpError::IOError(ref err) => write!(f, "IO error: {}", err),
+            TmpError::FloatError(ref err) => write!(f, "Parse error: {}", err),
+            TmpError::RegexError(ref err) => write!(f, "Parse error: {}", err),
+            TmpError::Utf8Error(ref err) => write!(f, "Parse error: {}", err),
+            TmpError::Unknow => write!(f, "Unknow error"),
+        }
+    }
+}
+
+
+fn read_sensor() -> Result<f32, TmpError> {
 
     let data = search_sensors();
     for path in data {
-        let mut file = File::open(path).unwrap();
+        let mut file = File::open(path)?;
         let mut text: Vec<u8> = Vec::with_capacity(75);
 
-        let re_crc = Regex::new(r"(YES)").unwrap();
+        let re_crc = Regex::new(r"(YES)")?;
 
-        let _ = file.read_to_end(&mut text);
+        file.read_to_end(&mut text)?;
 
-        if re_crc.is_match(std::str::from_utf8(&text).unwrap()) {
-            let re_temp = Regex::new(r"t=(-?\d+)").unwrap();
+        if re_crc.is_match(std::str::from_utf8(&text)?) {
+            let re_temp = Regex::new(r"t=(-?\d+)")?;
 
-            match re_temp.captures(std::str::from_utf8(&text).unwrap()).unwrap().at(1) {
+            match re_temp.captures(std::str::from_utf8(&text)?).unwrap().at(1) {
                 Some(tmp) => {
-                    let temperature = f32::from_str(tmp).unwrap();
+                    let temperature = f32::from_str(tmp)?;
                     return Ok(temperature / 1000.0);
                 }
                 None => {}
@@ -31,7 +108,7 @@ fn read_sensor() -> Result<f32, ()> {
         }
     }
 
-    Err(())
+    Err(TmpError::Unknow)
 
 }
 fn search_sensors() -> Vec<PathBuf> {
@@ -41,5 +118,5 @@ fn search_sensors() -> Vec<PathBuf> {
 
 
 fn main() {
-    println!("{:?}", read_sensor());
+    println!("{}", read_sensor().unwrap());
 }
