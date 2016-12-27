53dcc7f068db3c8911c0d4260d4d1cc69cf0a519
Samuel Dolt <samuel@dolt.ch>
2016-12-27 14:49:31 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
 extern crate glob;
+extern crate regex;
 
 use std::path::PathBuf;
+use regex::Regex;
+use std::fs::File;
+use std::io::Read;
 
 fn search_sensors() -> Vec<PathBuf> {
     glob::glob("/sys/bus/w1/devices/28-*/w1_slave").unwrap().filter_map(Result::ok).collect()
 }
 
 fn main() {
+    let data = search_sensors();
+    for path in data {
+        let mut file = File::open(path).unwrap();
+        let mut text: Vec<u8> = Vec::with_capacity(75);
+
+        let re_crc = Regex::new(r"(YES)").unwrap();
+
+        file.read_to_end(&mut text);
+
+        if re_crc.is_match(std::str::from_utf8(&text).unwrap()) {
+            let re_temp = Regex::new(r"t=(-?\d+)").unwrap();
+
+            match re_temp.captures(std::str::from_utf8(&text).unwrap()).unwrap().at(0) {
+                Some(tmp) => println!("{}", tmp),
+                None => {}
+            };
+        }
 
 
-
+    }
     println!("{:?}", search_sensors());
 }
